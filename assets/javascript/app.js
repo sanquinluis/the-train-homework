157e7923eda43b6086cff85fec4510f5361f2de5
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-09 17:33:13 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,18 +15,27 @@ $(document).ready(function(){
             firstTrain: firstTrain,
             frequency: frequency
 
-        });
+        })
+        return false;
 
    	 });
    	
-   	database.ref().on('child_added',function(Snapshot){
+   	database.ref().on('child_added',function(snapshot){
+   		var trainName = snapshot.val().trainName;
+   		var destination = snapshot.val().destination;
+   		var firstTrain = snapshot.val().firstTrain;
+   		var frequency = snapshot.val().frequency;
 		
-		$('table').append('<tr><td>' + Snapshot.val().trainName + '</td></tr>');
-		$('table').append('<tr><td>' + Snapshot.val().destination + '</td></tr>');
-
+		$('table').append('<tr><td>' + snapshot.val().trainName + 
+			'</td></td>' + destination  + 
+			'</td></td>' + firstTrain +
+			'</td></td>' + frequency);
 		
-		console.log(Snapshot.val().trainName);
-		console.log(Snapshot.val().destination);
+		console.log(snapshot.val());
+		console.log(snapshot.val().trainName);
+		console.log(snapshot.val().destination);
+		console.log(snapshot.val().firstTrain);
+		console.log(snapshot.val().frequency);
 		
     });
    	
