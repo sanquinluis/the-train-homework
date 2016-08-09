49d1770b67cf2646c0b58a40aa7c75bafa11c133
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-09 02:46:41 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-   
-   
+  var database = firebase.database();
+
+   $('#addUser').on("click", function(){
+
+   		var trainName = $("#trainInput").val().trim();
+        var destination = $("#destinationInput").val().trim();
+        var firstTrain = $("#firsttrainInput").val().trim();
+        var frequency = $("#frequencyInput").val().trim();
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency
+
+        });
+
+   	 });
+
+
+
+
+
+
+
 
 
 
