5945245b0cc890b037df8faf37a78860589efebd
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-15 14:56:21 -0400

    fixing some bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,15 +22,26 @@ $(document).ready(function(){
    	 });
    	
    	database.ref().on('child_added',function(snapshot){
-   		
-   		var m = moment();
-   		var chfirstTrain = m.moment().subtract(1,'day');
+   		//Time input of the frequency of train.
    		var chFrequency = snapshot.val().frequency;
+   		//live time is real time.
+   		var liveTime = moment();
+   		var chfirstTrain = moment(liveTime);
+   		//one month back to avoid live time.
+   		var oneMonth = moment(snapshot.val().firstTrain, 'hh:mm').subtract(1, 'month');
+   		var timeOftrain = moment(oneMonth).format('HH:mm:ss');
+   		//another one, to track the time of the train by the time put on Input.
+   		var anotherMonth = moment(timeOftrain,'hh:mm' ).subtract(1, 'month');
+   		var residualTime = moment().diff(moment(anotherMonth), 'minutes');
+   		var timeLeft = residualTime % chFrequency;
+   		var whereIstheTrain = chFrequency - timeLeft; 
+   		var hereIstheTrain = moment().add(whereIstheTrain, 'minutes').format('HH:mm:ss');
+
 
 		//Appending the child_add database into a tbody table when the name is inserted in the input.
 		$('#theTimeSchedule > tbody').append('<tr><td>' + snapshot.val().trainName + '</td><td>' +
-			snapshot.val().destination + '</td><td>' + snapshot.val().chFrequency + '</td><td>' +
-			snapshot.val().chfirstTrain + '</td></tr>');
+			snapshot.val().destination + '</td><td>' + snapshot.val().frequency + '</td><td>' +
+			snapshot.val().chfirstTrain + '</td></td>' + hereIstheTrain + '</td></tr>');
 
 
 	
@@ -40,10 +51,15 @@ $(document).ready(function(){
 		console.log(snapshot.val().destination);
 		console.log(snapshot.val().firstTrain);
 		console.log(snapshot.val().frequency);
+		console.log(snapshot.val().chFrequency);
 		console.log(chfirstTrain);
-		console.log(moment);
-		
-		
+		console.log(liveTime);
+		console.log(oneMonth);
+		console.log(timeOftrain);
+		console.log(anotherMonth);
+		console.log(residualTime);
+		console.log('chFrequency - timeLeft ' + whereIstheTrain);
+		console.log('where is the Train ' + hereIstheTrain);
     });
    	
 
